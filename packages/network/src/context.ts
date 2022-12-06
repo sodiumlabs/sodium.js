5c118b2dee6397934d857a1c1ed1e9a363e30228
AlbertHuang <r.albert.huang@gmail.com>
2022-12-06 10:31:09 +0800

    chore: Update sodium context

--- packages/network/src/context.ts
+++ packages/network/src/context.ts
@@ -15,7 +15,7 @@ export interface WalletContext {
 
 export const sodiumContext = {
   entryPointAddress: "0xC8Ddd44bB33BBC2c8DCcB0BB958DdE47363D5C28",
-  singletonAddress: "0xCaDae6742D024f45662c9ca7e15B4c58Cf3a00c7",
+  singletonAddress: "0xA5512e27D4F5178DA33750647944F7496b608F55",
   defaultHandlerAddress: "0xd8CBB2C610f4877547077752c99B7D5ce2E5F115",
   walletCreatorAddress: "0x34D0F455e9fC681ab9896CeeA5f1A5acbFb4497a",
 
